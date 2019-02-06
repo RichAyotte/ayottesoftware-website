2f233170e816ba6fd37be528b65d05e7d0921388
Richard Ayotte <rich@ayottesoftware.com>
2019-02-06 16:57:48 -0500

    Add headshot and refine style a bit. Move links to top

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,28 @@
 <section class="container">
 	<img class="background" src="~assets/ayotte-software-logo-greyscale-rotated90.svg"/>
 	<div>
+		<img
+			class="headshot"
+			height="200"
+			src="~assets/headshot.jpg"
+			width="200"
+		/>
 		<h1 class="title">Richard Ayotte</h1>
-		<h2 class="subtitle">Web Software Artist</h2>
+		<h2 class="subtitle">Full Stack Developer</h2>
+		<div class="links">
+			<a
+				class="button"
+				href="https://www.linkedin.com/in/richayotte/"
+			>LinkedIn</a>
+			<a
+				class="button"
+				href="https://stackoverflow.com/story/richayotte"
+			>Stack Exchange</a>
+			<a
+				class="button"
+				href="https://github.com/RichAyotte"
+			>Github</a>
+		</div>
 <pre class="card"><code>const i = {
 	paints: [<strong>
 		`JavaScript`
@@ -30,20 +50,6 @@
 	</strong>]
 }
 </code></pre>
-		<div class="links">
-			<a
-				class="button"
-				href="https://stackoverflow.com/story/richayotte"
-			>Stack Exchange</a>
-			<a
-				class="button"
-				href="https://github.com/RichAyotte"
-			>Github</a>
-			<a
-				class="button"
-				href="https://www.linkedin.com/in/richayotte/"
-			>LinkedIn</a>
-		</div>
 	</div>
 </section>
 </template>
@@ -52,12 +58,15 @@
 @import '~assets/variables'
 
 .background
-	height 100vh
-	opacity 0.8
-	position fixed
-	right 0
-	top 0
-	z-index -100
+	display none
+	@media $screen-xl
+		display block
+		height 100vh
+		opacity 0.8
+		position fixed
+		right 0
+		top 0
+		z-index -100
 
 .container
 	align-items center
@@ -66,32 +75,44 @@
 	min-height 100vh
 	text-align center
 
+.headshot
+	border-radius 5px
+	box-shadow 10px 10px 5px 0px rgba(0,0,0,0.2)
+	display block
+	margin 25px auto
+	transform rotate(-8deg)
+
 .title
 	font-family 'Gnuolane'
 	font-size responsive 40px 100px
 	font-weight 400
 	letter-spacing 1px
 	margin-top 15px
+	display inline-block
 
 .subtitle
 	font-size responsive 26px 42px
 	font-weight 300
 	letter-spacing responsive 4px 15px
+	white-space nowrap
+	clear both
 
 .links
-	display inline-block
-	margin 15px 0
+	margin 15px
+	a
+		border-radius 5px
+		display inline-block
+		margin 5px
 
 .card
 	background-color brand-orange
-	border 20px solid darken(brand-orange, 20%)
+	border 5px solid darken(brand-orange, 20%)
+	border-radius 5px
 	color lighten(brand-orange, 50)
 	display block
 	font-size responsive 12px 16px
 	margin 15px 0
 	padding 20px
-	-moz-tab-size 4
-	-o-tab-size 4
 	tab-size 4
 	text-align left
 	strong
